20e944dd5e7dd475cd4645d4e634371ef0b3a7e5
Parth-Agarwal216 <118837763+Parth-Agarwal216@users.noreply.github.com>
2024-01-21 20:07:06 +0530

    Update recipes.py

--- recipes.py
+++ recipes.py
@@ -1,21 +1,21 @@
 from imports import *
 from calorie_req import *
 
-recipe_dataset = pd.read_csv('batch1.csv')
+recipe_dataset = pd.read_csv("Final_Recipies.csv")
 
-def recipes_from_dataset(target_weight, number_of_days, age, weight, height, gender, pa, meal_type = None, diet_type = "Balanced"):
+def recipes_from_dataset(target_weight, number_of_days, age, weight, height, gender, pa, is_veg, meal_type = None, diet_type = "Balanced"):
     if meal_type is None:
         s_factor = 1
         done = False
         counter = 0
-        breakfast_set = recipe_dataset[recipe_dataset['meal_time'] == "breakfast"]
-        lunch_set = recipe_dataset[recipe_dataset['meal_time'] == "lunch"]
-        dinner_set = recipe_dataset[recipe_dataset['meal_time'] == "dinner"]
+        breakfast_set = recipe_dataset[(recipe_dataset['meal_time'] == "Breakfast") & (recipe_dataset['Diet Type'] == diet_type) & (recipe_dataset['Veg'] == is_veg)]
+        lunch_set = recipe_dataset[(recipe_dataset['meal_time'] == "Lunch") & (recipe_dataset['Diet Type'] == diet_type) & (recipe_dataset['Veg'] == is_veg)]
+        dinner_set = recipe_dataset[(recipe_dataset['meal_time'] == "Dinner") & (recipe_dataset['Diet Type'] == diet_type) & (recipe_dataset['Veg'] == is_veg)]
         while(counter < 20):
             counter += 1
-            breakfast = breakfast_set[breakfast_set["Diet Type"] == diet_type].sample(1)
-            lunch = lunch_set[breakfast_set["Diet Type"] == diet_type].sample(1)
-            dinner = dinner_set[breakfast_set["Diet Type"] == diet_type].sample(1)
+            breakfast = breakfast_set.sample(1)
+            lunch = lunch_set.sample(1)
+            dinner = dinner_set.sample(1)
             s_factor, done = check_meal(breakfast,lunch, dinner, diet_type, target_weight, number_of_days, age, weight, height, gender, pa) 
             if(done):
                 return breakfast, lunch, dinner, s_factor
